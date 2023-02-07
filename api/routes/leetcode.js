6440cd305847c576f012399508bc568e1eb522f5
Ayushman Sinha <ayushman.sinhaa@gmail.com>
2023-02-07 21:27:30 +0530

    someFIxes

--- api/routes/leetcode.js
+++ api/routes/leetcode.js
@@ -7,7 +7,13 @@ try{
         const response = await axios.get(URL);  
         const num_of_users=response.data.user_num;//Total Number of users taking part in the contest
         const total_pages=Math.ceil(num_of_users/25);//Total number of pages        
-        res.send(response.data);
+        //console.log(response.data.total_rank);
+        let result=[];
+        for(let i=1;i<=total_pages;i++){
+            const response_each = await axios.get(URL+"?pagination="+i);
+            result.push(response_each.data);
+        }
+        res.send(result);
     });
 }
 catch(err){
--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
-import React, { useEffect } from 'react'
+import  { useState,useEffect } from 'react'
 import axios from 'axios'
 
 const App = () => {
+  const [data, setData] = useState([])
   useEffect(() => {
     const res=axios.get('http://localhost:3500/api/routes/leetcode')
+    setData(res.data)
+    console.log(res.data)
   }, [])
   return (
     <div>
