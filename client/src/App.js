6a725be5048f614c56da7277e164e48eacd52959
Midas847 <iamronnie847@gmail.com>
2023-02-07 22:04:47 +0530

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,14 @@
-import  { useState,useEffect } from 'react'
-import axios from 'axios'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
   useEffect(() => {
-    const res=axios.get('http://localhost:3500/api/routes/leetcode')
-    setData(res.data)
-    console.log(res.data)
-  }, [])
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    const res = axios.get("http://localhost:3500/api/routes/leetcode");
+    setData(res.data);
+    console.log(res.data);
+  }, []);
+  return <div></div>;
+};
 
-export default App
+export default App;
