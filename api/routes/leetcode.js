e8e145a10177f4d1bce52d2995a3fe54d5866890
Ayushman Sinha <77104799+ayushman-sinha@users.noreply.github.com>
2023-02-08 03:51:53 +0530

    rating Fetch some fixes

--- api/routes/leetcode.js
+++ api/routes/leetcode.js
@@ -9,14 +9,25 @@ router.get("/", async (req, res) => {
     const total_pages = Math.ceil(num_of_users / 25); //Total number of pages
     //console.log(total_pages);
     let result = [];
-    for (let i = 1; i <= total_pages; i++) {
+    for (let i = 1; i <= 2; i++) {
       const response_each = await axios.get(URL + "?pagination="+i+"&region=global");
       // console.log(response_each.data.total_rank);
       JSON.stringify(response_each.data.total_rank);
       var username_list = [];
-      response_each.data.total_rank.map((item) => {
-        username_list.push(item.username);
-      });
+      var rating_fetch;
+      //Fix this part
+    //    response_each.data.total_rank.map((item) => {
+    //     const query_url='https://leetcode.com/graphql?query=query{userContestRanking(username:'+item.username+') { rating }}'
+        
+    //     username_list.push(item.username+" "+rating_fetch);
+    //   });
+    for(const item of response_each.data.total_rank){
+        let user_name="uwi";
+        let query_url='https://leetcode.com/graphql?query=query{userContestRanking(username: "ayushman_sinha"){rating}}'
+        const response_rating=await axios.get(query_url);
+
+        username_list.push(item.username+" "+response_rating.data.data.userContestRanking.rating);
+    }
       result.push(username_list);
     }
    
